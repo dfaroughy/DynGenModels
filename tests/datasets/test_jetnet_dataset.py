84ec132b937200d2a5c54bf36352cfa6e012ba1b
dfaroughy <63310570+dfaroughy@users.noreply.github.com>
2023-08-13 15:07:06 +0200

    new test: jetnet dataset

--- tests/datasets/test_jetnet_dataset.py
+++ tests/datasets/test_jetnet_dataset.py
@@ -1,47 +1,65 @@
 import pytest
-
 from DynGenModels.datamodules.jetnet.datasets import JetNetDataset
 
-SAMPLE_DIR_PATH = '../data/jetnet30' 
-SAMPLE_DATASETS = {'jetnet' : ['t.h5', 'particle features']}  
-SAMPLE_CLASS_LABELS = {'jetnet' : -1}  
-PARTICLE_FEATURES = ['eta_rel', 'phi_rel', 'pt_rel', 'R']
-PREPROCESS = ['standardize'] 
+SAMPLE_DIR_PATH = '/Users/dario/Dropbox/PROJECTS/ML/DynGenModels/data/jetnet' 
+SAMPLE_DATASETS = {'jetnet30' : ['t30.hdf5', 'particle_features'], 'jetnet150' : ['t150.hdf5', 'particle_features']}  
+SAMPLE_CLASS_LABELS = {'jetnet30' : 0, 'jetnet150' : 1}  
+PARTICLE_FEATURES = ['eta_rel', 'phi_rel', 'pt_rel']
+PREPROCESS = None
 MAX_NUM_JETS = 10
-MAX_NUM_PARTICLES = 30
+MAX_NUM_CONSTITUENTS = 20
 REMOVE_NEGATIVE_PT = True
-COMPUTE_JET_FEATURES =  False
 
-
-# 1. Instantiation Test
+#...1 Instantiation Test
 def test_instantiation():
-    dataset = JetNetDataset(dir_path=SAMPLE_DIR_PATH, datasets=SAMPLE_DATASETS, class_labels=SAMPLE_CLASS_LABELS, particle_features=PARTICLE_FEATURES, preprocess=PREPROCESS, max_num_jets=MAX_NUM_JETS, max_num_particles=MAX_NUM_PARTICLES, remove_negative_pt=REMOVE_NEGATIVE_PT, compute_jet_features=COMPUTE_JET_FEATURES)
+    dataset = JetNetDataset(dir_path=SAMPLE_DIR_PATH, datasets=SAMPLE_DATASETS, class_labels=SAMPLE_CLASS_LABELS, max_num_jets=MAX_NUM_JETS)
     assert isinstance(dataset, JetNetDataset)
 
-# 2. Length Test
-def test_len():
-    dataset = JetNetDataset(dir_path=SAMPLE_DIR_PATH, datasets=SAMPLE_DATASETS, class_labels=SAMPLE_CLASS_LABELS, particle_features=PARTICLE_FEATURES, preprocess=PREPROCESS, max_num_jets=MAX_NUM_JETS, max_num_particles=MAX_NUM_PARTICLES, remove_negative_pt=REMOVE_NEGATIVE_PT, compute_jet_features=COMPUTE_JET_FEATURES)
-    assert len(dataset) == MAX_NUM_JETS  
+#...2 Iterability Test
+@pytest.mark.parametrize("PREPROCESS", [None, ['compute_jet_features', 'standardize']]) 
+def test_iterability(PREPROCESS):
+    dataset = JetNetDataset(dir_path=SAMPLE_DIR_PATH, 
+                            datasets=SAMPLE_DATASETS, 
+                            class_labels=SAMPLE_CLASS_LABELS, 
+                            particle_features=PARTICLE_FEATURES,
+                            preprocess=PREPROCESS, 
+                            max_num_jets=MAX_NUM_JETS,
+                            max_num_constituents=MAX_NUM_CONSTITUENTS,
+                            remove_negative_pt=REMOVE_NEGATIVE_PT)
+    for jet in dataset:
+        assert 'label' in jet
+        assert 'mask' in jet
+        assert 'particle_features' in jet
+        if PREPROCESS is not None and 'compute_jet_features' in PREPROCESS: assert 'jet_features' in jet
+        else: assert 'jet_features' not in jet
 
-# 3. Data Retrieval Test
-def test_data_retrieval():
-    dataset = JetNetDataset(dir_path=SAMPLE_DIR_PATH, datasets=SAMPLE_DATASETS, class_labels=SAMPLE_CLASS_LABELS, particle_features=PARTICLE_FEATURES, preprocess=PREPROCESS, max_num_jets=MAX_NUM_JETS, max_num_particles=MAX_NUM_PARTICLES, remove_negative_pt=REMOVE_NEGATIVE_PT, compute_jet_features=COMPUTE_JET_FEATURES)
-    data = dataset[0]
-    assert 'label' in data
-    assert 'mask' in data
-    assert 'particle_features' in data
+#...3 Length Test
+@pytest.mark.parametrize("MAX_NUM_JETS", [10, 100]) 
+def test_len(MAX_NUM_JETS):
+    dataset = JetNetDataset(dir_path=SAMPLE_DIR_PATH, 
+                            datasets=SAMPLE_DATASETS, 
+                            class_labels=SAMPLE_CLASS_LABELS, 
+                            particle_features=PARTICLE_FEATURES,
+                            preprocess=PREPROCESS, 
+                            max_num_jets=MAX_NUM_JETS,
+                            max_num_constituents=MAX_NUM_CONSTITUENTS,
+                            remove_negative_pt=REMOVE_NEGATIVE_PT)
+    assert len(dataset) == MAX_NUM_JETS * len(SAMPLE_DATASETS)
 
-# 4. Iterability Test
-def test_iterability():
-    dataset = JetNetDataset(dir_path=SAMPLE_DIR_PATH, datasets=SAMPLE_DATASETS, class_labels=SAMPLE_CLASS_LABELS, particle_features=PARTICLE_FEATURES, preprocess=PREPROCESS, max_num_jets=MAX_NUM_JETS, max_num_particles=MAX_NUM_PARTICLES, remove_negative_pt=REMOVE_NEGATIVE_PT, compute_jet_features=COMPUTE_JET_FEATURES)
-    for data in dataset:
-        assert 'label' in data
-
-# 5. Saving and Loading Test
-def test_save_and_load(tmpdir):
-    dataset = JetNetDataset(dir_path=SAMPLE_DIR_PATH, datasets=SAMPLE_DATASETS, class_labels=SAMPLE_CLASS_LABELS, particle_features=PARTICLE_FEATURES, preprocess=PREPROCESS, max_num_jets=MAX_NUM_JETS, max_num_particles=MAX_NUM_PARTICLES, remove_negative_pt=REMOVE_NEGATIVE_PT, compute_jet_features=COMPUTE_JET_FEATURES)
-    save_path = tmpdir.mkdir("data").join("sample_save_path")
-    dataset.save(str(save_path))
-    loaded_dataset = JetNetDataset.load(str(save_path))
-    assert isinstance(loaded_dataset, JetNetDataset)
-    assert len(loaded_dataset) == len(dataset)
+#...4 Data retrieval amd shape Test
+@pytest.mark.parametrize("PARTICLE_FEATURES",  [['eta_rel', 'phi_rel', 'pt_rel'], ['eta_rel', 'phi_rel', 'pt_rel', 'e_rel', 'R']])
+@pytest.mark.parametrize("MAX_NUM_CONSTITUENTS",  [20, 30])
+def test_data_shapesl(PARTICLE_FEATURES, MAX_NUM_CONSTITUENTS, PREPROCESS=['compute_jet_features']):
+    dataset = JetNetDataset(dir_path=SAMPLE_DIR_PATH, 
+                            datasets=SAMPLE_DATASETS, 
+                            class_labels=SAMPLE_CLASS_LABELS, 
+                            particle_features=PARTICLE_FEATURES,
+                            preprocess=PREPROCESS, 
+                            max_num_jets=MAX_NUM_JETS,
+                            max_num_constituents=MAX_NUM_CONSTITUENTS,
+                            remove_negative_pt=REMOVE_NEGATIVE_PT)
+    jet = dataset[0]
+    assert jet['label'] in SAMPLE_CLASS_LABELS.values()
+    assert jet['mask'].shape == (MAX_NUM_CONSTITUENTS,)
+    assert jet['particle_features'].shape == (MAX_NUM_CONSTITUENTS, len(PARTICLE_FEATURES)) 
+    if PREPROCESS is not None and 'compute_jet_features' in PREPROCESS: assert jet['jet_features'].shape == (5,)
