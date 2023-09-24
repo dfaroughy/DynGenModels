aea454d9602bc0388c427218154ce9ef6d096429
dfaroughy <63310570+dfaroughy@users.noreply.github.com>
2023-09-24 15:12:08 -0400

    add option of using test source data for sampling

--- src/DynGenModels/pipelines/FlowMatchPipeline.py
+++ src/DynGenModels/pipelines/FlowMatchPipeline.py
@@ -1,42 +1,58 @@
 import torch
 from torchdyn.core import NeuralODE
+from tqdm.auto import tqdm
 
-from DynGenModels.datamodules.fermi.process import PostprocessData
+from DynGenModels.datamodules.fermi.dataprocess import PostProcessData
 from DynGenModels.trainer.trainer import FlowMatchTrainer
 from DynGenModels.pipelines.utils import TorchdynWrapper
 
 class FlowMatchPipeline:
     
     def __init__(self, 
-                 source_data: torch.Tensor=None,
-                 pretrained_model: FlowMatchTrainer=None, 
+                 trained_model: FlowMatchTrainer=None, 
+                 source_input: torch.Tensor=None,
                  solver: str='euler',
-                 sampling_steps: int=100):
+                 num_sampling_steps: int=100,
+                 sensitivity: str='adjoint',
+                 atol: float=1e-4,
+                 rtol: float=1e-4):
         
+        ''' if no source_input is given, then the source 'test' in dataloader is used as input for the ODE solver
+        '''
         super().__init__()
 
-        self.source = source_data
-        self.net = pretrained_model.dynamics.model
-        self.stats = pretrained_model.dataloader.datasets.summary_statistics['dataset']
-        self.postprocess = ['inverse_' + method for method in pretrained_model.dataloader.datasets.preprocess_methods[::-1]]
-        self.time = torch.linspace(0, 1, sampling_steps)
+        self.model = trained_model
+        self.source = source_input
+        self.net = self.model.dynamics.net
+        self.stats = self.model.dataloader.datasets.summary_statistics
+        self.postprocess_methods = ['inverse_' + method for method in self.model.dataloader.datasets.preprocess_methods[::-1]]
+        self.time = torch.linspace(0, self.model.dynamics.T, num_sampling_steps)
         self.solver = solver
+        self.sensitivity = sensitivity
+        self.atol = atol
+        self.rtol = rtol
         self.trajectories = self.ODEsolver()
         self.target = self.trajectories[-1]
 
     @torch.no_grad()
-    def ODEsolver(self, sensitivity="adjoint", atol=1e-4, rtol=1e-4):
+    def ODEsolver(self):
         node = NeuralODE(vector_field=TorchdynWrapper(self.net), 
                         solver=self.solver, 
-                        sensitivity=sensitivity, 
+                        sensitivity=self.sensitivity, 
                         seminorm=True if self.solver=='dopri5' else False,
-                        atol=atol if self.solver=='dopri5' else None, 
-                        rtol=rtol if self.solver=='dopri5' else None
+                        atol=self.atol if self.solver=='dopri5' else None, 
+                        rtol=self.rtol if self.solver=='dopri5' else None
                         )
-        trajectories = node.trajectory(x=self.source, t_span=self.time)
+        if self.source is None:
+            trajectories = []
+            for batch in tqdm(self.model.dataloader.test, desc="sampling"):
+                trajectories.append(node.trajectory(x=batch['source'], t_span=self.time))
+            trajectories = torch.cat(trajectories, dim=-1)
+        else:
+            trajectories = node.trajectory(x=self.source, t_span=self.time)
         return self.post_process(trajectories)
 
     def post_process(self, trajectories):
-        sample = PostprocessData(data=trajectories, stats=self.stats, methods=self.postprocess)
+        sample = PostProcessData(data=trajectories, stats=self.stats, methods=self.postprocess_methods)
         sample.postprocess()
         return sample.galactic_features
