f6bf01e01d2dde3e8d11f7631d6774143684dc35
dfaroughy <63310570+dfaroughy@users.noreply.github.com>
2023-09-24 20:57:27 -0400

    restructured dataset and and processing

--- src/DynGenModels/datamodules/fermi/dataprocess.py
+++ src/DynGenModels/datamodules/fermi/dataprocess.py
@@ -1,79 +1,173 @@
+from typing import Any
 import torch
 import numpy as np
 
-class FormatData:
-
-    def __init__(self, 
-                 data: torch.Tensor=None,
-                 cuts: dict=None):
-                 
-        self.data = data
-        self.cuts = cuts if cuts is not None else {'theta': None, 'phi': None, 'energy': None}
-
-    def selection_cuts(self, feature, cut=None):
-        if cut is None: cut=[-np.inf, np.inf]
-        dic={'theta':0, 'phi':1, 'energy':2}
-        mask = (self.data[..., dic[feature]] >= cut[0]) & (self.data[..., dic[feature]] <= cut[1])
-        self.data = self.data[mask]
-        return self
-
-    def format(self):
-        self.selection_cuts(feature='energy', cut=self.cuts['energy'])
-        self.selection_cuts(feature='theta', cut=self.cuts['theta'])
-        self.selection_cuts(feature='phi', cut=self.cuts['phi'])
-
-class PreProcessData:
+class PreProcessFermiData:
 
     def __init__(self, 
                  data, 
-                 stats,
+                 cuts: dict=None,
                  methods: list=['standardize']
                  ):
         
         self.galactic_features = data
-        self.mean, self.std, self.min, self.max = stats 
+        self.cuts = cuts if cuts is not None else {'theta': None, 'phi': None, 'energy': None}
         self.methods = methods
+        self.summary_stats = {}
 
     def preprocess(self):
+        #...apply data selection cuts
+        self.selection_cuts(feature='energy', cut=self.cuts['energy'])
+        self.selection_cuts(feature='theta', cut=self.cuts['theta'])
+        self.selection_cuts(feature='phi', cut=self.cuts['phi'])
+        
+        #...preprocess with provided methods
         for method in self.methods:
             method = getattr(self, method, None)
             if method and callable(method): method()
             else: raise ValueError('Preprocessing method {} not implemented'.format(method))
-        
+    
+    def selection_cuts(self, feature, cut=None):
+        if cut is None: cut=[-np.inf, np.inf]
+        dic={'theta':0, 'phi':1, 'energy':2}
+        mask = (self.galactic_features[..., dic[feature]] >= cut[0]) & (self.galactic_features[..., dic[feature]] <= cut[1])
+        self.galactic_features = self.galactic_features[mask]
+
     def standardize(self,  sigma: float=1.0):
-        self.galactic_features = (self.galactic_features - self.mean) * (sigma / self.std )
+        """ standardize data to have zero mean and unit variance
+        """
+        self.summary_stats['mean'] = torch.mean(self.galactic_features, dim=0)
+        self.summary_stats['std'] = torch.std(self.galactic_features, dim=0)
+        self.galactic_features = (self.galactic_features - self.summary_stats['mean']) * (sigma / self.summary_stats['std'])
 
     def normalize(self):
-        self.galactic_features = (self.galactic_features - self.min) / ( self.max - self.min )
+        """ normalize data to unit interval
+        """
+        self.summary_stats['min'], _ = torch.min(self.galactic_features, dim=0)
+        self.summary_stats['max'], _ = torch.max(self.galactic_features, dim=0)
+        self.galactic_features = (self.galactic_features - self.summary_stats['min']) / ( self.summary_stats['max'] - self.summary_stats['min'])
     
-    def logit_transform(self, alpha=1e-6):
+    def logit_transform(self, alpha=1e-5):
+        """ smoothen rectified distribution with logit transform
+        """
         self.galactic_features = self.galactic_features * (1 - 2 * alpha) + alpha
         self.galactic_features = torch.log(self.galactic_features / (1 - self.galactic_features))
 
-class PostProcessData:
+
+
+class PostProcessFermiData:
 
     def __init__(self, 
                  data, 
-                 stats,
+                 summary_stats,
                  methods: list=['inverse_standardize']
                  ):
         
         self.galactic_features = data
-        self.mean, self.std, self.min, self.max = stats 
+        self.summary_stats = summary_stats
         self.methods = methods
 
     def postprocess(self):
         for method in self.methods:
             method = getattr(self, method, None)
             if method and callable(method): method()
-            else: raise ValueError('Preprocessing method {} not implemented'.format(method))
-        
+            else: raise ValueError('Postprocessing method {} not implemented'.format(method))
+
     def inverse_standardize(self,  sigma: float=1.0):
-        self.galactic_features = self.galactic_features * (self.std / sigma) + self.mean
+        self.galactic_features = self.galactic_features * (self.summary_stats['std'] / sigma) + self.summary_stats['mean']
 
     def inverse_normalize(self):
-        self.galactic_features = self.galactic_features * ( self.max - self.min ) + self.min
+        self.galactic_features = self.galactic_features * (self.summary_stats['max'] - self.summary_stats['min']) + self.summary_stats['min']
     
-    def inverse_logit_transform(self, alpha=1e-6):
-        self.galactic_features = 1 / (1 + torch.exp(-self.galactic_features))
+    def inverse_logit_transform(self, alpha=1e-5):
+        exp = torch.exp(self.galactic_features)
+        self.galactic_features = exp / (1 + exp)
         self.galactic_features = (self.galactic_features - alpha) / (1 - 2 * alpha)
+
+
+
+
+# class FormatData:
+
+#     def __init__(self, 
+#                  data: torch.Tensor=None,
+#                  cuts: dict=None):
+                 
+#         self.data = data
+#         self.cuts = cuts if cuts is not None else {'theta': None, 'phi': None, 'energy': None}
+
+#     def selection_cuts(self, feature, cut=None):
+#         if cut is None: cut=[-np.inf, np.inf]
+#         dic={'theta':0, 'phi':1, 'energy':2}
+#         mask = (self.data[..., dic[feature]] >= cut[0]) & (self.data[..., dic[feature]] <= cut[1])
+#         self.data = self.data[mask]
+#         return self
+
+#     def format(self):
+#         self.selection_cuts(feature='energy', cut=self.cuts['energy'])
+#         self.selection_cuts(feature='theta', cut=self.cuts['theta'])
+#         self.selection_cuts(feature='phi', cut=self.cuts['phi'])
+
+# class PreProcessData:
+
+#     def __init__(self, 
+#                  data, 
+#                  stats,
+#                  methods: list=['standardize']
+#                  ):
+        
+#         self.galactic_features = data
+#         self.mean, self.std, self.min, self.max = stats 
+#         self.methods = methods
+
+#     def preprocess(self):
+#         for method in self.methods:
+#             method = getattr(self, method, None)
+#             if method and callable(method): method()
+#             else: raise ValueError('Preprocessing method {} not implemented'.format(method))
+        
+#     def standardize(self,  sigma: float=1.0):
+#         self.mean = torch.mean(self.galactic_features, dim=0)
+#         self.std = torch.std(self.galactic_features, dim=0)
+#         self.galactic_features = (self.galactic_features - self.mean) * (sigma / self.std )
+
+#     def normalize(self):
+#         self.min, _ = torch.min(data, dim=0)
+#         self.max, _ = torch.max(data, dim=0)
+#         self.galactic_features = (self.galactic_features - self.min) / ( self.max - self.min )
+    
+#     def logit_transform(self, alpha=1e-6):
+#         self.galactic_features = self.galactic_features * (1 - 2 * alpha) + alpha
+#         self.galactic_features = torch.log(self.galactic_features / (1 - self.galactic_features))
+
+#     def save_stats(self):
+#         return (self.mean, self.std, self.min, self.max)
+    
+
+# class PostProcessData:
+
+#     def __init__(self, 
+#                  data, 
+#                  stats,
+#                  methods: list=['inverse_standardize']
+#                  ):
+        
+#         self.galactic_features = data
+#         self.mean, self.std, self.min, self.max = stats 
+#         self.methods = methods
+
+#     def postprocess(self):
+#         for method in self.methods:
+#             method = getattr(self, method, None)
+#             if method and callable(method): method()
+#             else: raise ValueError('Preprocessing method {} not implemented'.format(method))
+        
+#     def inverse_standardize(self,  sigma: float=1.0):
+#         self.galactic_features = self.galactic_features * (self.std / sigma) + self.mean
+
+#     def inverse_normalize(self):
+#         self.galactic_features = self.galactic_features * ( self.max - self.min ) + self.min
+    
+#     def inverse_logit_transform(self, alpha=1e-6):
+#         self.galactic_features = 1 / (1 + torch.exp(-self.galactic_features))
+#         self.galactic_features = (self.galactic_features - alpha) / (1 - 2 * alpha)
--- src/DynGenModels/datamodules/fermi/datasets.py
+++ src/DynGenModels/datamodules/fermi/datasets.py
@@ -2,7 +2,8 @@ import torch
 import numpy as np
 from torch.utils.data import Dataset
 
-from DynGenModels.datamodules.fermi.dataprocess import FormatData, PreProcessData
+from DynGenModels.datamodules.fermi.dataprocess import PreProcessFermiData
+
 
 class FermiDataset(Dataset):
 
@@ -17,19 +18,19 @@ class FermiDataset(Dataset):
         self.dataset = dataset
         self.cuts = cuts
         self.preprocess_methods = preprocess 
-        self.summary_statistics = None
+        self.summary_stats = None
         
         ''' datasets:
             target data (t=1) :  fermi galaxy data
             source data (t=0) :  std gaussian
-            '''
+        '''
 
-        self.target = self.fermi_data()
-        self.source = torch.rand_like(self.target, dtype=self.target.dtype)
+        self.target = self.get_fermi_data()
+        self.source = torch.randn_like(self.target, dtype=self.target.dtype)
 
     def __getitem__(self, idx):
         output = {}
-        output['target'] = self.fermi_preprocessing(self.target[idx]) if self.preprocess_methods is not None else self.target[idx]
+        output['target'] = self.target[idx]
         output['source'] = self.source[idx]
         return output
 
@@ -40,30 +41,11 @@ class FermiDataset(Dataset):
         for i in range(len(self)):
             yield self[i]
 
-    def fermi_data(self):
-        dataset = torch.tensor(np.load(self.path+'/'+self.dataset), dtype=torch.float32)
-        dataset = self.fermi_formatting(dataset)
-        self.summary_statistics = self.summary_stats(dataset)
+    def get_fermi_data(self):
+        data_raw = torch.tensor(np.load(self.path+'/'+self.dataset), dtype=torch.float32)
+        data = PreProcessFermiData(data_raw, cuts=self.cuts, methods=self.preprocess_methods)
+        data.preprocess()
+        self.summary_stats = data.summary_stats
         print("INFO: loading and preprocessing data...")
-        print('\t- dataset: {} \n \t- shape: {}'.format(self.path, dataset.shape))
-        return dataset
-
-    def fermi_formatting(self, sample):
-        sample = FormatData(sample, cuts=self.cuts)
-        sample.format()
-        return sample.data
-    
-    def fermi_preprocessing(self, sample):
-        sample = PreProcessData(data=sample, 
-                                stats=self.summary_statistics,
-                                methods=self.preprocess_methods
-                                )
-        sample.preprocess()
-        return sample.galactic_features
-    
-    def summary_stats(self, data):
-        mean = torch.mean(data, dim=0)
-        std = torch.std(data, dim=0)
-        min, _ = torch.min(data, dim=0)
-        max, _ = torch.max(data, dim=0)
-        return (mean, std, min, max)
+        print('\t- dataset: {} \n \t- shape: {}'.format(self.path, data.galactic_features.shape))
+        return data.galactic_features
