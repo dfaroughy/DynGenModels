df007e7af52fbec421033ac2d6073192f8b17654
dfaroughy <63310570+dfaroughy@users.noreply.github.com>
2023-10-05 11:14:15 -0400

    updated experiment

--- experiments/fermi.py
+++ experiments/fermi.py
@@ -1,76 +1,71 @@
-import matplotlib.pyplot as plt
+import torch
+import numpy as np
+import matplotlib.pyplot as plt 
 
 from DynGenModels.trainer.trainer import DynGenModelTrainer
 from DynGenModels.configs.fermi_configs import FermiGCE_MLP_CondFlowMatch as Configs
 
 configs = Configs(dataset = '../data/fermi/fermi_data_galactic_coord.npy',
                   features = ['theta', 'phi', 'energy'],
-                  preprocess = ['normalize', 'logit_transform', 'standardize'],
-                  cuts = {'theta': [-20., 20.], 'phi': [4., 10.], 'energy': [1000, 2000]},
-                  data_split_fracs = [0.6, 0.1, 0.3],
-                  epochs = 1000,
-                  early_stopping=20,
-                  batch_size = 512,
-                  warmup_epochs = 20,
+                  preprocess=['normalize', 'logit_transform', 'standardize'], 
+                  cuts = {'theta': [-10., 10.], 'phi': [4., 10.], 'energy': [1000, 2000]},
+                  data_split_fracs = [1.0, 0.0, 0.0],
+                  epochs = 10000,
+                  batch_size = 2048,
                   lr = 1e-3,
-                  dim_hidden = 128, 
-                  device = 'cpu',
-                  sigma = 1e-4,
-                  solver='euler',
-                  num_sampling_steps=500,
-                  seed = 12345)
+                  dim_hidden = 256, 
+                  sigma = 0.0,
+                  solver='midpoint',
+                  num_sampling_steps=500)
 
 #...set working directory for results:
 
 configs.set_workdir(path='../results', save_config=True)
 
-#...define dataset :
+#...define dataset and model:
 
 from DynGenModels.datamodules.fermi.datasets import FermiDataset 
 from DynGenModels.datamodules.fermi.dataloader import FermiDataLoader 
-
-dataset = FermiDataset(configs)
-
-#...Train model:
-
 from DynGenModels.models.deep_nets import MLP
 from DynGenModels.dynamics.cnf.flowmatch import SimplifiedCondFlowMatching
 
+dataset = FermiDataset(configs)
+dataloader = FermiDataLoader(dataset, configs)
 net = MLP(configs)
-cfm = DynGenModelTrainer(dynamics=SimplifiedCondFlowMatching(net, configs), 
-                        dataloader=FermiDataLoader(dataset, configs),
-                        configs=configs)
+dynamics = SimplifiedCondFlowMatching(net, configs)
+cfm = DynGenModelTrainer(dynamics=dynamics, dataloader=dataloader, configs=configs)
+
+#...train model:
 
 cfm.train()
 
-#...sample from model
+#...sample from model:
 
 from DynGenModels.pipelines.SamplingPipeline import FlowMatchPipeline 
-from DynGenModels.datamodules.fermi.dataprocess import PostProcessFermiData
+from DynGenModels.datamodules.fermi.dataprocess import PostProcessFermiData 
 
 pipeline = FlowMatchPipeline(trained_model=cfm, 
+                             source_input=torch.randn(50000, 3),
                              configs=configs, 
-                             postprocessor=PostProcessFermiData,
-                             solver='dopri5',
-                             num_sampling_steps=100)
+                             postprocessor=PostProcessFermiData)
 
 
-#...plots
+coord = [r'$\theta$', r'$\phi$', r'$E$']
+color=['gold', 'darkblue', 'darkred']
 
-bins=150 
 fig, ax = plt.subplots(1, 3, figsize=(9, 3))
-
-ax[0].hist(x=pipeline.target[:,0], color='r', bins=bins, histtype='step', lw=0.75, density=True)
-ax[0].hist(x=dataset.target[:,0], color='k', bins=bins, histtype='stepfilled', alpha=0.25, lw=0, density=True)
-
-ax[1].hist(x=pipeline.target[:,1], color='r', bins=bins, histtype='step', lw=0.75, density=True)
-ax[1].hist(x=dataset.target[:,1], color='k', bins=bins, histtype='stepfilled', alpha=0.25, lw=0, density=True)
-
-ax[2].hist(x=pipeline.target[:,2], color='r', bins=bins, histtype='step', lw=0.75, density=True)
-ax[2].hist(x=dataset.target[:,2], color='k', bins=bins, histtype='stepfilled', alpha=0.25, lw=0,density=True)
-
-ax[0].set_xlabel(r'$\theta$')
-ax[1].set_xlabel(r'$\phi$')
-ax[2].set_xlabel(r'$E$')
+for i in range(3):
+    ax[i].hist(dataset.target[..., i], bins=100, color='silver', density=True)
+    ax[i].hist(pipeline.target[..., i], bins=100, color=color[i], histtype='step', density=True)
+    ax[i].set_xlabel(coord[i])
 plt.tight_layout()
-plt.show()
+plt.savefig(configs.workdir +'/fermi_coords.pdf')
+
+fig, ax = plt.subplots(1, 3, figsize=(9, 3))
+for i, j in [(0,1), (1,2), (2,0)]:
+    ax[i].scatter(dataset.target[:50000, i], dataset.target[:50000, j], s=1, alpha=0.1, c='silver')
+    ax[i].scatter(pipeline.target[:50000, i], pipeline.target[:50000, j], s=1, alpha=0.1, c=color[i])
+    ax[i].set_xlabel(coord[i])
+    ax[i].set_ylabel(coord[j])
+plt.tight_layout()
+plt.savefig('configs.workdir'+'/fermi_coords_2D.pdf')
