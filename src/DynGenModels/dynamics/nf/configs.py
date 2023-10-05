e836583c04ec67be32a40928aad5c9b649c10c22
dfaroughy <63310570+dfaroughy@users.noreply.github.com>
2023-10-05 11:13:53 -0400

    rename config

--- src/DynGenModels/dynamics/nf/configs.py
+++ src/DynGenModels/dynamics/nf/configs.py
@@ -2,9 +2,11 @@ from dataclasses import dataclass
 
 @dataclass
 class NormFlow_Configs:
+    dynamics_name: str = 'NormFlow'
     num_transforms: int = 8
 
 @dataclass
 class Deconvolution_NormFlow_Configs:
+    dynamics_name: str = 'DeconvolutionNormFlow'
     num_transforms: int = 5
     num_mc_draws: int = 30
