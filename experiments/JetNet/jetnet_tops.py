ee4f1a7032729c4b63643b43f65f0995fd0a7ce7
dfaroughy <63310570+dfaroughy@users.noreply.github.com>
2023-10-08 22:59:56 -0400

    update

--- experiments/JetNet/jetnet_tops.py
+++ experiments/JetNet/jetnet_tops.py
@@ -11,7 +11,7 @@ configs = Configs(data_dir = '../../data/jetnet',
                   num_particles = 30,
                   jet_types = 't',
                   data_split_fracs = [0.8, 0.2, 0.0],
-                  epochs = 10000,
+                  epochs = 3,
                   batch_size = 1024,
                   lr = 1e-3,
                   dim_hidden = 32, 
@@ -50,23 +50,24 @@ pipeline = FlowMatchPipeline(trained_model=cfm,
 
 #...plot generated data:
 
-def feature_plots(jetnet_data, generated=None, features=[r'$\Delta\eta$', r'$\Delta\phi$', r'$p^{\rm rel}_T$'], num_particles=100000, color='blue'):
+def feature_plots(jetnet_data, generated=None, save_dir=None, features=[r'$\Delta\eta$', r'$\Delta\phi$', r'$p^{\rm rel}_T$'], num_particles=100000, color='blue'):
     _, axs = plt.subplots(1, 3, figsize=(7, 2.5))
     axs[0].hist(jetnet_data[..., 0].flatten()[:num_particles], bins=100,log=True, color='gold' if generated is None else 'silver', density=True)
     if generated is not None:
-        axs[0].hist(generated[..., 0].flatten()[:num_particles], bins=100,log=True, color='gold', histtype='step', density=True) 
+        axs[0].hist(generated[..., 0].flatten()[:num_particles], bins=100,log=True, color='gold', histtype='step', density=True, lw=0.75) 
     axs[1].hist(jetnet_data[..., 1].flatten()[:num_particles], bins=100, log=True, color='darkblue' if generated is None else 'silver', density=True)
     if generated is not None:
-        axs[1].hist(generated[..., 1].flatten()[:num_particles], bins=100,log=True, color='darkblue', histtype='step', density=True)
+        axs[1].hist(generated[..., 1].flatten()[:num_particles], bins=100,log=True, color='darkblue', histtype='step', density=True, lw=0.75)
     axs[2].hist(jetnet_data[..., 2].flatten()[:num_particles], bins=100, log=True, color='darkred' if generated is None else 'silver', density=True)
     if generated is not None:
-        axs[2].hist(generated[..., 2].flatten()[:num_particles], bins=100,log=True, color='darkred', histtype='step', density=True)
+        axs[2].hist(generated[..., 2].flatten()[:num_particles], bins=100,log=True, color='darkred', histtype='step', density=True, lw=0.75)
     axs[0].set_xlabel(features[0])
     axs[1].set_xlabel(features[1])
     axs[2].set_xlabel(features[2])
     axs[0].set_ylabel('counts')
     plt.tight_layout()
-    plt.show()
+    if save_dir is not None:
+        plt.savefig(save_dir + '/particle_features.pdf')
 
 
-feature_plots(tops.particles, pipeline.target)
+feature_plots(tops.particles, pipeline.target, save_dir=configs.workdir)
