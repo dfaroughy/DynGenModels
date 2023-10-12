fix cuda for normalizing flows

experiments/Fermi-LAT/fermi_NF.py
@@ -1,84 +1,132 @@
-import torch
-import numpy as np
-import matplotlib.pyplot as plt 
 
 from DynGenModels.trainer.trainer import DynGenModelTrainer
-from DynGenModels.configs.fermi_configs import FermiGCE_Couplings_RQS_NormFlow as Configs
+from DynGenModels.configs.fermi_configs import FermiGCE_MAF_RQS_NormFlow as Configs
 
 configs = Configs(dataset = '../../data/fermi/fermi_data_galactic_coord.npy',
-                  features = ['theta', 'phi', 'energy'],
-                  preprocess =['normalize', 'logit_transform', 'standardize'], 
-                  cuts = {'theta': [-10., 10.], 'phi': [4., 10.], 'energy': [1000, 2000]},
-                  data_split_fracs = [0.8, 0.2, 0.0],
-                  epochs = 10000,
-                  warmup_epochs = 100,
-                  early_stopping = 100,
-                  print_epochs = 100, 
-                  batch_size = 2048,
-                  lr = 1e-3,
-                  num_transforms = 6,
-                  num_blocks = 3,
-                  mask = 'checkerboard',
-                  dim_hidden = 256, 
-                  use_batch_norm = True,
-                  num_bins = 20,
-                  tail_bound = 10, 
-                  num_gen_samples = 100000,
-                  device = 'cuda:0')
+                features = ['theta', 'phi', 'energy'],
+                preprocess = ['normalize', 'logit_transform', 'standardize'],
+                cuts = {'theta': [-10., 10.], 'phi': [-5., 10.], 'energy': [1000, 2000]},
+                data_split_fracs = [0.8, 0.2, 0.0],
+                EPOCHS = 10000,
+                batch_size = 15000,
+                print_epochs = 20,
+                early_stopping = 100,
+                min_epochs = 1000,
+                lr = 1e-4,
+                dim_hidden = 256, 
+                dropout = 0.1,
+                DEVICE = 'cuda:0',
+                optimizer = 'Adam',
+                seed = 12345,
+                tail_bound = 10, 
+                num_transforms = 10,
+                num_blocks = 3,
+                num_bins = 30,
+                num_gen_samples = 1000000
+                )
 
 #...set working directory for results:
 
 configs.set_workdir(path='../../results', save_config=True)
 
-#...define dataset and model:
+#...define dataset :
 
 from DynGenModels.datamodules.fermi.datasets import FermiDataset 
 from DynGenModels.datamodules.fermi.dataloader import FermiDataLoader 
-from DynGenModels.models.nflow_nets import CouplingsPiecewiseRQS 
+from DynGenModels.models.nflow_nets import MAFPiecewiseRQS
 from DynGenModels.dynamics.nf.normflows import NormalizingFlow
 
 fermi = FermiDataset(configs)
 dataloader = FermiDataLoader(fermi, configs)
-net = CouplingsPiecewiseRQS(configs)
+net = MAFPiecewiseRQS(configs)
 dynamics = NormalizingFlow(net, configs)
-nf = DynGenModelTrainer(dynamics=dynamics, dataloader=dataloader, configs=configs)
+maf = DynGenModelTrainer(dynamics=dynamics, dataloader=dataloader, configs=configs)
 
 #...train model:
 
-nf.train()
+maf.train()
 
 #...sample from model:
 
 from DynGenModels.pipelines.SamplingPipeline import NormFlowPipeline
 from DynGenModels.datamodules.fermi.dataprocess import PostProcessFermiData 
 
-pipeline = NormFlowPipeline(trained_model=nf, 
+pipeline = NormFlowPipeline(trained_model=maf, 
                             configs=configs, 
                             postprocessor=PostProcessFermiData,
                             best_epoch_model=False)
 
-pipeline_best = NormFlowPipeline(trained_model=nf, 
+pipeline_best = NormFlowPipeline(trained_model=maf, 
                                  configs=configs, 
                                  postprocessor=PostProcessFermiData,
                                  best_epoch_model=True)
 
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+def results_plots(data, generated=None, save_dir=None, features=[r'$\theta$', r'$\phi$', r'$E$ [GeV]'], num_particles=None):
+    num_particles = 100000000 if num_particles is None else num_particles
+    fig = plt.figure(figsize=(12, 4))
+    gs = gridspec.GridSpec(2, 3, height_ratios=[5, 1])
+    gs.update(hspace=0.1) 
+    
+    for idx, feature in enumerate(features):
+        ax = fig.add_subplot(gs[idx])
+        h1, bins, _ = ax.hist(data[..., idx].flatten()[:num_particles], bins=100, color='silver', density=True)
+        if generated is not None:
+            h2, _, _ = ax.hist(generated[..., idx].flatten()[:num_particles], bins=100, color=['gold', 'darkblue', 'darkred'][idx], histtype='step', density=True, lw=0.75)
+            ax.set_xticklabels([])
+            ax.set_xticks([])
+            for tick in ax.yaxis.get_major_ticks():
+               tick.label.set_fontsize(8)
+        else:
+            ax.set_xlabel(feature)
+        
+        # Ratio plot
+        if generated is not None:
+            ax_ratio = fig.add_subplot(gs[idx + 3])
+            ratio = np.divide(h1, h2, out=np.ones_like(h2), where=h2 != 0)
+            ax_ratio.plot(0.5 * (bins[:-1] + bins[1:]), ratio, color=['gold', 'darkblue', 'darkred'][idx],lw=0.75)
+            ax_ratio.set_ylim(0.5, 1.5, 0) # Adjust this as needed
+            ax_ratio.set_xlabel(feature)
+            ax_ratio.axhline(1, color='gray', linestyle='--', lw=0.75)
+            for tick in ax_ratio.xaxis.get_major_ticks():
+               tick.label.set_fontsize(7)
+            for tick in ax_ratio.yaxis.get_major_ticks():
+              tick.label.set_fontsize(5)  
+            if idx == 0:
+                ax_ratio.set_ylabel('ratio', fontsize=8)
+            ax_ratio.set_yticks([0.5, 1, 1.5])
+    if save_dir is not None:
+        plt.savefig(save_dir)
+    plt.show()
+
+#...plot results:
+
+results_plots(fermi.target, generated=pipeline.target, save_dir=configs.workdir + '/fermi_features.pdf', features=[r'$\theta$', r'$\phi$', r'$E$ [GeV]'])
+results_plots(fermi.target, generated=pipeline_best.target, save_dir=configs.workdir + '/fermi_features_best.pdf', features=[r'$\theta$', r'$\phi$', r'$E$ [GeV]'])
+
+#...plot 2D projections:
+
 coord = [r'$\theta$', r'$\phi$', r'$E$']
 color=['gold', 'darkblue', 'darkred']
-
-fig, ax = plt.subplots(1, 3, figsize=(9, 3))
-for i in range(3):
-    ax[i].hist(fermi.target[..., i], bins=100, color='silver', density=True)
-    ax[i].hist(pipeline.target[..., i], bins=100, color=color[i], histtype='step', density=True)
-    ax[i].hist(pipeline_best.target[..., i], bins=100, color=color[i], histtype='step', ls=':',density=True)
-    ax[i].set_xlabel(coord[i])
-plt.tight_layout()
-plt.savefig(configs.workdir + '/fermi_coords.pdf')
-
 fig, ax = plt.subplots(1, 3, figsize=(9, 3))
 for i, j in [(0,1), (1,2), (2,0)]:
     ax[i].hexbin(pipeline.target[..., i], pipeline.target[..., j], cmap='plasma', gridsize=200)
     ax[i].set_xlabel(coord[i])
     ax[i].set_ylabel(coord[j])
 plt.tight_layout()
+plt.savefig(configs.workdir + '/fermi_features_2D.pdf')
 plt.show()
-plt.savefig(configs.workdir + '/fermi_coords_2D.pdf')
+
+
+fig, ax = plt.subplots(1, 3, figsize=(9, 3))
+for i, j in [(0,1), (1,2), (2,0)]:
+    ax[i].hexbin(pipeline_best.target[..., i], pipeline_best.target[..., j], cmap='plasma', gridsize=200)
+    ax[i].set_xlabel(coord[i])
+    ax[i].set_ylabel(coord[j])
+plt.tight_layout()
+plt.savefig(configs.workdir + '/fermi_features_2D_best.pdf')
+plt.show()

src/DynGenModels/dynamics/nf/deconvolution.py
@@ -10,13 +10,14 @@ from copy import deepcopy
 class DeconvolutionNormFlows:
 
 	def __init__(self, net, configs: dataclass):
+		self.device = configs.DEVICE
 		self.num_mc_draws = configs.num_mc_draws
 		self.num_transforms = configs.num_transforms
 		self.flow_net = net
 		self.permutation = ReversePermutation(features=configs.dim_input)
 		self.transforms()
-		self.flows = CompositeTransform(self.transforms)
-		self.base_distribution = StandardNormal(shape=[configs.dim_input])
+		self.flows = CompositeTransform(self.transforms).to(configs.DEVICE)
+		self.base_distribution = StandardNormal(shape=[configs.dim_input]).to(configs.DEVICE)
 		self.net = Flow(self.flows, self.base_distribution)
 
 	def transforms(self):
@@ -37,6 +38,7 @@ class DeconvolutionNormFlows:
 		epsilon = torch.randn_like(smeared)
 		epsilon = torch.reshape(epsilon,(-1, epsilon.dim(), 1)) 
 		x = smeared + torch.squeeze(torch.bmm(cov, epsilon))        # x = smeared - cov * epsilon
+		x = x.to(self.device)
 		logprob = torch.reshape(self.net.log_prob(x),(-1, self.num_mc_draws))
 		loss = - torch.mean(torch.logsumexp(logprob, dim=-1))
 		return loss + torch.log(torch.tensor(self.num_mc_draws))

src/DynGenModels/dynamics/nf/normflows.py
@@ -11,12 +11,13 @@ from copy import deepcopy
 class NormalizingFlow:
 
     def __init__(self, net, configs: dataclass):
+        self.device = configs.DEVICE
         self.num_transforms = configs.num_transforms
         self.flow_net = net
         self.permutation = ReversePermutation(features=configs.dim_input)
         self.transforms()
-        self.flows = CompositeTransform(self.transforms)
-        self.base_distribution = StandardNormal(shape=[configs.dim_input])
+        self.flows = CompositeTransform(self.transforms).to(configs.DEVICE)
+        self.base_distribution = StandardNormal(shape=[configs.dim_input]).to(configs.DEVICE)
         self.net = Flow(self.flows, self.base_distribution)
 
     def transforms(self):
@@ -30,7 +31,7 @@ class NormalizingFlow:
     def loss(self, batch):
         ''' Negative Log-probability loss
         '''
-        target = batch['target']
+        target = batch['target'].to(self.device)
         loss = - self.net.log_prob(target)
         return torch.mean(loss)
 