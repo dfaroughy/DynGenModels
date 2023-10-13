1176a5251d68a75c345f5109641d8977df4938be
dfaroughy <63310570+dfaroughy@users.noreply.github.com>
2023-10-13 11:03:23 -0400

    update

--- experiments/Fermi-LAT/fermi_NF.py
+++ experiments/Fermi-LAT/fermi_NF.py
@@ -1,6 +1,6 @@
 
 from DynGenModels.trainer.trainer import DynGenModelTrainer
-from DynGenModels.configs.fermi_configs import FermiGCE_MAF_RQS_NormFlow as Configs
+from DynGenModels.configs.fermi_configs import FermiGCE_Couplings_RQS_NormFlow as Configs
 
 configs = Configs(DATA ='FermiGCE',
                 dataset = '../../data/fermi/fermi_data_galactic_coord.npy',
@@ -8,7 +8,7 @@ configs = Configs(DATA ='FermiGCE',
                 preprocess = ['normalize', 'logit_transform', 'standardize'],
                 cuts = {'theta': [-10., 10.], 'phi': [-5., 10.], 'energy': [1000, 2000]},
                 data_split_fracs = [0.8, 0.2, 0.0],
-                EPOCHS = 10000,
+                EPOCHS = 10,
                 batch_size = 15000,
                 print_epochs = 10,
                 early_stopping = 100,
@@ -18,7 +18,7 @@ configs = Configs(DATA ='FermiGCE',
                 dropout = 0.1,
                 DEVICE = 'cuda:1',
                 optimizer = 'Adam',
-                seed = 12345,
+                fix_seed = 12345,
                 tail_bound = 10, 
                 num_transforms = 10,
                 num_blocks = 3,
@@ -34,30 +34,30 @@ configs.set_workdir(path='../../results', save_config=True)
 
 from DynGenModels.datamodules.fermi.datasets import FermiDataset 
 from DynGenModels.datamodules.fermi.dataloader import FermiDataLoader 
-from DynGenModels.models.nflow_nets import MAFPiecewiseRQS
+from DynGenModels.models.nflow_nets import CouplingsPiecewiseRQS
 from DynGenModels.dynamics.nf.normflows import NormalizingFlow
 
 fermi = FermiDataset(configs)
 dataloader = FermiDataLoader(fermi, configs)
-net = MAFPiecewiseRQS(configs)
+net = CouplingsPiecewiseRQS(configs)
 dynamics = NormalizingFlow(net, configs)
-maf = DynGenModelTrainer(dynamics=dynamics, dataloader=dataloader, configs=configs)
+coupl = DynGenModelTrainer(dynamics=dynamics, dataloader=dataloader, configs=configs)
 
 #...train model:
 
-maf.train()
+coupl.train()
 
 #...sample from model:
 
 from DynGenModels.pipelines.SamplingPipeline import NormFlowPipeline
 from DynGenModels.datamodules.fermi.dataprocess import PostProcessFermiData 
 
-pipeline = NormFlowPipeline(trained_model=maf, 
+pipeline = NormFlowPipeline(trained_model=coupl, 
                             configs=configs, 
                             postprocessor=PostProcessFermiData,
                             best_epoch_model=False)
 
-pipeline_best = NormFlowPipeline(trained_model=maf, 
+pipeline_best = NormFlowPipeline(trained_model=coupl, 
                                  configs=configs, 
                                  postprocessor=PostProcessFermiData,
                                  best_epoch_model=True)
@@ -70,7 +70,7 @@ from utils import results_plots, results_2D_plots
 results_plots(data=fermi.target, 
               generated=pipeline.target, 
               comparator='pull',
-              model = 'MAF', 
+              model = 'Couplings RQS', 
               save_path=configs.workdir + '/fermi_features.pdf', 
               bins=300, 
               features=[r'$\theta$', r'$\phi$', r'$E$ [GeV]'])
@@ -78,7 +78,7 @@ results_plots(data=fermi.target,
 results_plots(data=fermi.target, 
               generated=pipeline_best.target, 
               comparator='pull',
-              model = 'MAF', 
+              model = 'Couplings RQS', 
               save_path=configs.workdir + '/fermi_features_best.pdf', 
               bins=300, 
               features=[r'$\theta$', r'$\phi$', r'$E$ [GeV]'])
