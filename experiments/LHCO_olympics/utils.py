c5d58b9330daa6fed720dc673e0f63d7b7b93960
Darius Faroughy <dariusfar@gmail.com>
2023-11-02 14:56:48 -0400

    color change

--- experiments/LHCO_olympics/utils.py
+++ experiments/LHCO_olympics/utils.py
@@ -18,9 +18,9 @@ def plot_jet_features(lhco, trajectories, feature, xlim, time_step=None, d_step=
 
     ax[0].hist(lhco.background[...,i], bins=bins, histtype='stepfilled', color='gold', label='SR background', log= bool_log, alpha=0.4)
     ax[0].hist(lhco.source[...,i], bins=bins, histtype='step', color='blue', label='SB1 source', log=bool_log)
-    if d_step > 0: ax[0].hist(trajectories[step - d_step][...,i], bins=bins, histtype='step', color='darkblue', label='CFM t={}'.format((step-d_step)/N), ls=':', lw=0.5, log=bool_log)
-    ax[0].hist(trajectories[step][...,i], bins=bins, histtype='step', color='blue', label='CFM t={}'.format(step/N), ls='--', lw=0.75, log=bool_log)
-    if d_step > 0: ax[0].hist(trajectories[step + d_step][...,i], bins=bins, histtype='step', color='darkblue', label='CFM t={}'.format((step+d_step)/N), ls=':',  lw=0.5,log=bool_log)
+    if d_step > 0: ax[0].hist(trajectories[step - d_step][...,i], bins=bins, histtype='step', color='purple', label='CFM t={}'.format((step-d_step)/N), ls=':', lw=0.5, log=bool_log)
+    ax[0].hist(trajectories[step][...,i], bins=bins, histtype='step', color='purple', label='CFM t={}'.format(step/N), ls='--', lw=0.75, log=bool_log)
+    if d_step > 0: ax[0].hist(trajectories[step + d_step][...,i], bins=bins, histtype='step', color='purple', label='CFM t={}'.format((step+d_step)/N), ls=':',  lw=0.5,log=bool_log)
     ax[0].hist(trajectories[-1][...,i], bins=bins, histtype='step', color='red', label='CFM t=1', ls='--', lw=0.5,log=bool_log)
     ax[0].hist(lhco.target[...,i], bins=bins, histtype='step', color='red', label='SB2 target', log=bool_log)
     ax[0].set_xlabel(r'${}$ leading jet'.format(feature))
@@ -28,9 +28,9 @@ def plot_jet_features(lhco, trajectories, feature, xlim, time_step=None, d_step=
 
     ax[1].hist(lhco.background[...,j], bins=bins, histtype='stepfilled', color='gold', log=bool_log, alpha=0.4, label='SR background')
     ax[1].hist(lhco.source[...,j], bins=bins, histtype='step', color='blue', log=bool_log, label='SB1 source')
-    if d_step > 0: ax[1].hist(trajectories[step - d_step][...,j], bins=bins, histtype='step', color='blue', ls=':', lw=0.5, log=bool_log, label='CFM t={}'.format((step-d_step)/N))
-    ax[1].hist(trajectories[step][...,j], bins=bins, histtype='step', color='blue', ls='--', lw=0.75,log=bool_log, label='CFM t={}'.format(step/N))
-    if d_step > 0: ax[1].hist(trajectories[step + d_step][...,j], bins=bins, histtype='step', color='blue', ls=':', lw=0.5, log=bool_log, label='CFM t={}'.format((step+d_step)/N))
+    if d_step > 0: ax[1].hist(trajectories[step - d_step][...,j], bins=bins, histtype='step', color='purple', ls=':', lw=0.5, log=bool_log, label='CFM t={}'.format((step-d_step)/N))
+    ax[1].hist(trajectories[step][...,j], bins=bins, histtype='step', color='purple', ls='--', lw=0.75,log=bool_log, label='CFM t={}'.format(step/N))
+    if d_step > 0: ax[1].hist(trajectories[step + d_step][...,j], bins=bins, histtype='step', color='purple', ls=':', lw=0.5, log=bool_log, label='CFM t={}'.format((step+d_step)/N))
     ax[1].hist(trajectories[-1][...,j], bins=bins, histtype='step', color='red', ls='--', lw=0.5, log= bool_log, label='CFM t=1')
     ax[1].hist(lhco.target[...,j], bins=bins, histtype='step', color='red', log=bool_log, label='SB2 target')
     ax[1].set_xlabel(r'${}$ subleading jet'.format(feature))
@@ -60,9 +60,9 @@ def plot_dijet_mass(lhco, trajectories, bins=np.arange(2700, 6000, 20), time_ste
 
     ax.hist(mjj_sr_bckgrd, bins=bins, histtype='stepfilled', color='gold', label='SR background', log=True, alpha=0.4,  density=True)
     ax.hist(mjj_sb1, bins=bins, histtype='step', color='blue', label='SB1 source', log=True, density=True)
-    if d_step > 0: ax.hist(mjj_sr_early, bins=bins, histtype='step', color='blue', ls=':', lw = 0.5, label='CFM, t={}'.format((step-d_step)/N), log=True, density=True)
-    ax.hist(mjj_sr_middle, bins=bins, histtype='step', color='blue', ls='--', lw = 0.5, label='CFM, t={}'.format(step/N), log=True, density=True)
-    if d_step > 0: ax.hist(mjj_sr_late, bins=bins, histtype='step', color='blue', ls=':', lw = 0.5, label='CFM, t={}'.format((step+d_step)/N), log=True, density=True)
+    if d_step > 0: ax.hist(mjj_sr_early, bins=bins, histtype='step', color='purple', ls=':', lw = 0.5, label='CFM, t={}'.format((step-d_step)/N), log=True, density=True)
+    ax.hist(mjj_sr_middle, bins=bins, histtype='step', color='purple', ls='--', lw = 0.5, label='CFM, t={}'.format(step/N), log=True, density=True)
+    if d_step > 0: ax.hist(mjj_sr_late, bins=bins, histtype='step', color='purple', ls=':', lw = 0.5, label='CFM, t={}'.format((step+d_step)/N), log=True, density=True)
     ax.hist(mjj(trajectories[-1]), bins=bins, histtype='step', color='red', ls='--', lw = 0.5, label='CFM, t=1.0', log=True, density=True)
     ax.hist(mjj_sb2, bins=bins, histtype='step', color='red', label='SB2 target', log=True, density=True)
     ax.set_xlabel(r'$m_{jj}$')
@@ -83,12 +83,12 @@ def mjj(data, window=[-np.inf, np.inf]):
 
     px_1 = pt_1 * torch.cos(phi_1)
     py_1 = pt_1 * torch.sin(phi_1)
-    pz_1 = pt_1 * torch.sinh(eta_1)
+    pz_1 = torch.sqrt(pt_1**2 + m_1**2) * torch.sinh(eta_1)
     e_1 = torch.sqrt(px_1**2 + py_1**2 + pz_1**2 + m_1**2)
 
     px_2 = pt_2 * torch.cos(phi_2)
     py_2 = pt_2 * torch.sin(phi_2)
-    pz_2 = pt_2 * torch.sinh(eta_2)
+    pz_2 = torch.sqrt(pt_2**2 + m_2**2) * torch.sinh(eta_2)
     e_2 = torch.sqrt(px_2**2 + py_2**2 + pz_2**2 + m_2**2)
 
     mjj = torch.sqrt((e_1 + e_2)**2 - (px_1 + px_2)**2 - (py_1 + py_2)**2 - (pz_1 + pz_2)**2)
