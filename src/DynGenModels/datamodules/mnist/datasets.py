39064481c6bcd210b5b7e5fa7652047f6d1b2e46
Darius Faroughy <dariusfar@gmail.com>
2024-02-13 21:20:17 -0500

    fix mnist dataset

--- src/DynGenModels/datamodules/mnist/datasets.py
+++ src/DynGenModels/datamodules/mnist/datasets.py
@@ -3,6 +3,7 @@ import numpy as np
 from torch.utils.data import Dataset
 from dataclasses import dataclass
 from torchvision import datasets, transforms
+from PIL import Image
 
 class MNISTDataset(Dataset):
 
@@ -27,53 +28,103 @@ class MNISTDataset(Dataset):
             yield self[i]
 
     def get_target_data(self):
-        
         if self.config.DATA_TARGET == 'mnist':
-            self.data_1 = datasets.MNIST(root='../../data', 
-                                         train=True, 
-                                         download=True, 
-                                         transform=transforms.ToTensor())
-        
+            train = datasets.MNIST(root='../../data', 
+                                   train=True, 
+                                   download=True, 
+                                   transform=transforms.ToTensor())
+            self.target_label = train.targets.tolist() 
+            self.target = train.data.unsqueeze(1).float() / 255.0 
+
         elif self.config.DATA_TARGET == 'emnist':
-            self.data_1 = datasets.EMNIST(root='../../data', 
+            train = datasets.EMNIST(root='../../data', 
+                                    split='letters', 
+                                    train=True, 
+                                    download=True, 
+                                    transform=transforms.ToTensor())  
+            self.target_label = train.targets.tolist()
+            train = train.data.unsqueeze(1).float() / 255.0 
+            train = torch.flip(train, dims=[3])
+            self.target = torch.rot90(train, 1, (2, 3))
+             
+        elif self.config.DATA_TARGET == 'fashion':
+            train = datasets.FashionMNIST(root='../../data', 
+                                        train=True, 
+                                        download=True, 
+                                        transform=transforms.ToTensor())
+            self.target_label = train.targets.tolist()
+            self.target = train.data.unsqueeze(1).float() / 255.0
+
+    def get_source_data(self):
+        if self.config.DATA_SOURCE == 'mnist':
+            train = datasets.MNIST(root='../../data', 
+                                   train=True, 
+                                   download=True, 
+                                   transform=transforms.ToTensor())
+            test = datasets.MNIST(root='../../data', 
+                                  train=False, 
+                                  download=True, 
+                                  transform=transforms.ToTensor())
+            self.source_label = train.targets.tolist() if self.config.DATA_SOURCE is not None else [0] * len(train)
+            self.source_test_label = test.targets.tolist() if self.config.DATA_SOURCE is not None else [0] * len(test)
+            self.source = train.data.unsqueeze(1).float() / 255.0 if self.config.DATA_SOURCE is not None else torch.rand_like(train.data.unsqueeze(1))
+            self.source_test = test.data.unsqueeze(1).float() / 255.0
+
+        elif self.config.DATA_SOURCE == 'emnist':
+            train = datasets.EMNIST(root='../../data', 
                                           split='letters', 
                                           train=True, 
                                           download=True, 
                                           transform=transforms.ToTensor())
-        
-        elif self.config.DATA_TARGET == 'fashion':
-             self.data_1 = datasets.FashionMNIST(root='../../data', 
-                                                 train=True, 
-                                                 download=True, 
-                                                 transform=transforms.ToTensor())
-        
-        self.target = [d[0] for d in self.data_1]
-        self.target_label = [d[1] for d in self.data_1]
-        
-    def get_source_data(self):
+            test = datasets.EMNIST(root='../../data', 
+                                          split='letters', 
+                                          train=False, 
+                                          download=True, 
+                                          transform=transforms.ToTensor())
+            self.source_label = train.targets.tolist() if self.config.DATA_SOURCE is not None else [0] * len(train)
+            self.source_test_label = test.targets.tolist() if self.config.DATA_SOURCE is not None else [0] * len(test)
+            train = train.data.unsqueeze(1).float() / 255.0 
+            train = torch.flip(train, dims=[3])
+            self.source = torch.rot90(train, 1, (2, 3))
+            test = test.data.unsqueeze(1).float() / 255.0
+            test = torch.flip(test, dims=[3])
+            self.source_test = torch.rot90(test, 1, (2, 3))
 
-        if self.config.DATA_SOURCE == 'mnist':
-            self.data_0 = datasets.MNIST(root='../../data', 
-                                         train=True, 
+        elif self.config.DATA_SOURCE == 'fashion':
+            train = datasets.FashionMNIST(root='../../data', 
+                                          train=True, 
+                                          download=True, 
+                                          transform=transforms.ToTensor())
+            test = datasets.FashionMNIST(root='../../data', 
+                                         train=False, 
                                          download=True, 
                                          transform=transforms.ToTensor())
-       
-        elif self.config.DATA_SOURCE == 'emnist':
-            self.data_0 = datasets.EMNIST(root='../../data', 
-                                          split='letters', 
-                                          train=True, 
-                                          download=True, 
-                                          transform=transforms.ToTensor())
-       
-        elif self.config.DATA_SOURCE == 'fashion':
-             self.data_0 = datasets.FashionMNIST(root='../../data', 
-                                                 train=True, 
-                                                 download=True, 
-                                                 transform=transforms.ToTensor())
-        else:
-            self.data_0 = self.data_1
+            self.source_label = train.targets.tolist() if self.config.DATA_SOURCE is not None else [0] * len(train)
+            self.source_test_label = test.targets.tolist() if self.config.DATA_SOURCE is not None else [0] * len(test)
+            self.source = train.data.unsqueeze(1).float() / 255.0 if self.config.DATA_SOURCE is not None else torch.rand_like(train.data.unsqueeze(1))
+            self.source_test = test.data.unsqueeze(1).float() / 255.0
 
-        self.source = [d[0] for d in self.data_0] if self.config.DATA_SOURCE is not None else [torch.rand_like(d[0]) for d in self.data_0]
-        self.source_label = [d[1] for d in self.data_0] if self.config.DATA_SOURCE is not None else [0] * len(self.data_0)
-        self.source = self.source[:len(self.target)]
-        self.source_label = self.source_label[:len(self.target)]
+        elif self.config.DATA_SOURCE == 'distorted_mnist':
+
+            train = datasets.MNIST(root='../../data', 
+                                   train=True, 
+                                   download=True, 
+                                   transform=transforms.ToTensor())
+                        
+            test = datasets.MNIST(root='../../data', 
+                                  train=False, 
+                                  download=True, 
+                                  transform=transforms.ToTensor())
+
+            self.source_label = train.targets.tolist() if self.config.DATA_SOURCE is not None else [0] * len(train)
+            self.source = train.data.unsqueeze(1) if self.config.DATA_SOURCE is not None else torch.rand_like(train.data.unsqueeze(1))
+            self.source_test = test.data.unsqueeze(1)
+            self.source_test_label = test.targets.tolist() if self.config.DATA_SOURCE is not None else [0] * len(test)
+
+            mask = torch.triu(torch.ones(28, 28), diagonal=0).unsqueeze(0).unsqueeze(0)
+            mask = mask.expand_as(self.source)  
+            self.source = self.source * mask
+
+            mask = torch.triu(torch.ones(28, 28), diagonal=0).unsqueeze(0).unsqueeze(0)
+            mask = mask.expand_as(self.source_test)  
+            self.source_test = self.source_test * mask
