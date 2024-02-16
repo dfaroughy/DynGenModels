0046fa1f848d3a53d9355e7b119a63642b30d551
Darius Faroughy <dariusfar@gmail.com>
2024-02-16 14:40:05 -0500

    update

--- experiments/JetClass/utils.py
+++ experiments/JetClass/utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import matplotlib.gridspec as gridspec
 
 def plot_jet_trajectories(trained_model, bins=[50,50,50], figsize=(15,5)):
 
@@ -27,4 +27,45 @@ def plot_jet_trajectories(trained_model, bins=[50,50,50], figsize=(15,5)):
     axs[0].set_ylabel('density')
     axs[2].legend(loc='upper right', fontsize=7)
     plt.tight_layout()
+    plt.show()
+
+
+def results_plots(trained_model, save_dir=None, features=[r'$p^{\rm rel}_T$', r'$\Delta\eta$', r'$\Delta\phi$'], num_particles=100000, figsize=(10, 3)):
+    
+    jet0 = trained_model.pipeline.trajectories[0].reshape(-1,3).detach().cpu().numpy()
+    jet1 = trained_model.pipeline.trajectories[-1].reshape(-1,3).detach().cpu().numpy()
+    jet_true = trained_model.dataset.target[:jet0.shape[0]].detach().cpu().numpy()
+    
+    fig = plt.figure(figsize=figsize)
+    gs = gridspec.GridSpec(2, 3, height_ratios=[5, 1])
+    gs.update(hspace=0.05) 
+    
+    for idx, feature in enumerate(features):
+        ax = fig.add_subplot(gs[idx])
+        h, bins, _ = ax.hist(jet_true[..., idx].flatten()[:num_particles], bins=100, log=True   , color='silver', density=True, label='truth')
+        h0, _, _ = ax.hist(jet0[..., idx].flatten()[:num_particles], bins=100, log=True, color=['gold', 'darkblue', 'darkred'][idx], ls='--', histtype='step', density=True, lw=0.75, label='source (t=0)')
+        h1, _, _ = ax.hist(jet1[..., idx].flatten()[:num_particles], bins=100, log=True, color=['gold', 'darkblue', 'darkred'][idx], histtype='step', density=True, lw=0.75 , label='target (t=1)')
+        ax.set_xticklabels([])
+        ax.set_xticks([])
+        for tick in ax.yaxis.get_major_ticks():
+            tick.label.set_fontsize(8)
+        ax.legend(fontsize=6)
+
+        # Ratio plot:
+        
+        ax_ratio = fig.add_subplot(gs[idx + 3])
+        ratio = np.divide(h1, h, out=np.ones_like(h), where=h != 0)
+        ax_ratio.plot(0.5 * (bins[:-1] + bins[1:]), ratio, color=['gold', 'darkblue', 'darkred'][idx],lw=0.75)
+        ax_ratio.set_ylim(0.5, 1.5, 0) # Adjust this as needed
+        ax_ratio.set_xlabel(feature)
+        ax_ratio.axhline(1, color='gray', linestyle='--', lw=0.75)
+        for tick in ax_ratio.xaxis.get_major_ticks():
+            tick.label.set_fontsize(7)
+        for tick in ax_ratio.yaxis.get_major_ticks():
+            tick.label.set_fontsize(5)  
+        if idx == 0:
+            ax_ratio.set_ylabel('ratio', fontsize=8)
+        ax_ratio.set_yticks([0.5, 1, 1.5])
+    if save_dir is not None:
+        plt.savefig(save_dir + '/particle_features.pdf')
     plt.show()
