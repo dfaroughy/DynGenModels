3452241afecc188e52298ecbd4f13d952f455d89
Darius Faroughy <dariusfar@gmail.com>
2024-02-24 18:39:56 -0500

    update

--- experiments/JetClass/train_model.py
+++ experiments/JetClass/train_model.py
@@ -2,15 +2,17 @@ from DynGenModels.configs.registered_experiments import Config_JetClass_EPiC_Con
 from DynGenModels.models.experiment import Experiment
 
 cfm = Experiment(Config_JetClass_EPiC_CondFlowMatch,
-                 NAME = 'noise_to_top',
-                 DATA_SOURCE = 'noise',
+                 NAME = 'qcd_to_top',
+                 DATA_SOURCE = 'qcd',
                  DATA_TARGET = 'top',
                  MAX_NUM_CONSTITUENTS = 30,
                  DYNAMICS = 'ContextSchrodingerBridgeCFM',
                  DATA_SPLIT_FRACS = [0.8, 0.2, 0.0],
                  PREPROCESS = ['standardize'],
                  BATCH_SIZE = 1024,
-                 EPOCHS = 1000,
+                 EPOCHS = 10000,
+                 MIN_EPOCHS = 1000,
+                 EARLY_STOPPING = 100,
                  PRINT_EPOCHS = 10,
                  LR = 1e-4,
                  GRADIENT_CLIP = 1.0,
@@ -23,7 +25,7 @@ cfm = Experiment(Config_JetClass_EPiC_CondFlowMatch,
                  SIGMA = 1e-5,
                  SOLVER ='midpoint',
                  NUM_SAMPLING_STEPS = 200,
-                 DEVICE = 'cuda:2')
+                 DEVICE = 'cuda:1')
 
 cfm.train()
 
@@ -33,3 +35,4 @@ from utils import plot_consitutents, plot_jets
 cfm.generate_samples(cfm.dataset.source_preprocess, Postprocessor=Postprocessor)
 plot_consitutents(cfm, save_dir=cfm.config.WORKDIR, features=[r'$p^{\rm rel}_T$', r'$\Delta\eta$', r'$\Delta\phi$'], figsize=(12,3.5))
 plot_jets(cfm, save_dir=cfm.config.WORKDIR, features=[r'$p_t$', r'$\eta$', r'$\phi$', r'$m$'], figsize=(12,3))
+
