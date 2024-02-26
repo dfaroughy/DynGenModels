3e99416a26c4702e05ecb79696bfc232cc763268
Darius Faroughy <dariusfar@gmail.com>
2024-02-26 17:15:37 -0500

    fix for noise -> jets

--- experiments/JetClass/sample_model.py
+++ experiments/JetClass/sample_model.py
@@ -6,9 +6,9 @@ from DynGenModels.datamodules.jetclass.dataprocess import PostProcessJetClassDat
 
 from utils import plot_consitutents, plot_jets 
 
-path = '../../results/' + sys.argv[1]
-cfm = Experiment(Config_JetClass_EPiC_CondFlowMatch, path=path, DEVICE='cuda:1', )
+path = '../../results/' + sys.argv[2]
+cfm = Experiment(Config_JetClass_EPiC_CondFlowMatch, path=path, DEVICE='cuda:'+str(sys.argv[1]))
 cfm.load(model='best')
-cfm.generate_samples(cfm.dataset.source_preprocess, Postprocessor=Postprocessor)
+cfm.generate_samples(cfm.dataset.source_preprocess[:10000], Postprocessor=Postprocessor)
 plot_consitutents(cfm, save_dir=path, features=[r'$p^{\rm rel}_T$', r'$\Delta\eta$', r'$\Delta\phi$'], figsize=(12,3.5))
-plot_jets(cfm, save_dir=path, features=[r'$p_t$', r'$\eta$', r'$\phi$', r'$m$'], figsize=(12,3))
+plot_jets(cfm, save_dir=path, plot_source=False, features=[r'$p_t$', r'$\eta$', r'$\phi$', r'$m$'], figsize=(12,3))
--- experiments/JetClass/utils.py
+++ experiments/JetClass/utils.py
@@ -22,43 +22,49 @@ def coord_transform(features, output_coords='pt_eta_phi_m'):
 
 
 def get_feats_constiutuents(massless_constituents):
-    x = massless_constituents.reshape(-1,3)
-    x = torch.cat([x, torch.zeros_like(x)[..., None, 0]], dim=-1) # add zero masses
+    if massless_constituents.shape[-1] == 3:
+        x = massless_constituents.reshape(-1,3)
+        x = torch.cat([x, torch.zeros_like(x)[..., None, 0]], dim=-1) # add zero masses
+    elif massless_constituents.shape[-1] == 4:
+        x = massless_constituents
+    else:
+        raise ValueError('Invalid shape for massless_constituents')
     x = coord_transform(x, output_coords='px_py_pz_e').reshape(-1, massless_constituents.shape[1],4).sum(-2) # sum over particles four-momenta
     return coord_transform(x, output_coords='pt_eta_phi_m') # convert to pt, eta, phi, m
-    
+
 
 def plot_consitutents(trained_model, 
-                        save_dir=None, 
-                        features=[r'$p^{\rm rel}_T$', r'$\Delta\eta$', r'$\Delta\phi$'], 
-                        bins=[np.arange(0,1,0.01), np.arange(-2.5, 2.5, 0.05), np.arange(-1, 1, 0.02)], 
-                        num_particles=100000, 
-                        figsize=(10, 3)):
+                    save_dir=None, 
+                    plot_source=True,
+                    features=[r'$p^{\rm rel}_T$', r'$\Delta\eta$', r'$\Delta\phi$'], 
+                    bins=[np.arange(0,1,0.01), np.arange(-2.5, 2.5, 0.05), np.arange(-1, 1, 0.02)], 
+                    color = ['gold', 'darkblue', 'darkred'],
+                    num_particles=100000, 
+                    figsize=(10, 3)):
     
-    jet0 = trained_model.pipeline.trajectories[0].reshape(-1,3).detach().cpu().numpy()
+    if plot_source: jet0 = trained_model.pipeline.trajectories[0].reshape(-1,3).detach().cpu().numpy()
     jet1 = trained_model.pipeline.trajectories[-1].reshape(-1,3).detach().cpu().numpy()
-    jet_true = trained_model.dataset.target[:jet0.shape[0]].detach().cpu().numpy()
+    jet_true = trained_model.dataset.target[:jet1.shape[0]].detach().cpu().numpy()
     
     fig = plt.figure(figsize=figsize)
-    gs = gridspec.GridSpec(2, 3, height_ratios=[5, 1])
-    gs.update(hspace=0.05) 
+    gs = gridspec.GridSpec(2, len(features), height_ratios=[5, 1])
+    gs.update(hspace=0.05)
     
     for idx, feature in enumerate(features):
         ax = fig.add_subplot(gs[idx])
         h, b, _ = ax.hist(jet_true[..., idx].flatten()[:num_particles], bins=bins[idx], log=True , color='silver', density=True, label='target (truth)')
-        h0, _, _ = ax.hist(jet0[..., idx].flatten()[:num_particles], bins=bins[idx], log=True, color=['gold', 'darkblue', 'darkred'][idx], ls='--', histtype='step', density=True, lw=0.75, label='source (t=0)')
-        h1, _, _ = ax.hist(jet1[..., idx].flatten()[:num_particles], bins=bins[idx], log=True, color=['gold', 'darkblue', 'darkred'][idx], histtype='step', density=True, lw=0.75 , label='gen. target (t=1)')
+        if plot_source: _, _, _ = ax.hist(jet0[..., idx].flatten()[:num_particles], bins=bins[idx], log=True, color=color[idx], ls='--', histtype='step', density=True, lw=0.75, label='source (t=0)')
+        h1, _, _ = ax.hist(jet1[..., idx].flatten()[:num_particles], bins=bins[idx], log=True, color=color[idx], histtype='step', density=True, lw=0.75 , label='gen. target (t=1)')
         ax.set_xticklabels([])
         ax.set_xticks([])
         for tick in ax.yaxis.get_major_ticks():
             tick.label.set_fontsize(8)
-        ax.legend(fontsize=6)
+        ax.legend(fontsize=6, loc='upper right')
 
-        # Ratio plot:
-        
+        #...ratio plot
         ax_ratio = fig.add_subplot(gs[idx + 3])
         ratio = np.divide(h1, h, out=np.ones_like(h), where=h != 0)
-        ax_ratio.plot(0.5 * (b[:-1] + b[1:]), ratio, color=['gold', 'darkblue', 'darkred'][idx],lw=0.75)
+        ax_ratio.plot(0.5 * (b[:-1] + b[1:]), ratio,  drawstyle='steps-post', color=color[idx],lw=0.75)
         ax_ratio.set_ylim(0.5, 1.5, 0) # Adjust this as needed
         ax_ratio.set_xlabel(feature)
         ax_ratio.axhline(1, color='gray', linestyle='--', lw=0.75)
@@ -73,34 +79,48 @@ def plot_consitutents(trained_model,
         plt.savefig(save_dir + '/particle_features.pdf')
     else:
         plt.show()
-
+        
 
 def plot_jets(trained_model, 
-                save_dir=None,
-                figsize=(12, 2),
-                features=[r'$p_t$', r'$\eta$', r'$\phi$', r'$m$'],
-                bins = [np.arange(0.9,1.1,0.001), 
-                        np.arange(-0.1,0.1,0.001), 
-                        np.arange(-0.1,0.1,0.001), 
-                        np.arange(0.,1,0.01)
-                        ]):
+              save_dir=None,
+              figsize=(12, 2),
+              plot_source=True,
+              features=[r'$p_t$', r'$\eta$', r'$\phi$', r'$m$'],
+              color=['gold', 'darkblue', 'darkred', 'purple'],
+              bins=[np.arange(0.9,1.1,0.001), np.arange(-0.1,0.1,0.001),  np.arange(-0.1,0.1,0.001),  np.arange(0.,1,0.01)
+             ]):
 
-    jet0 = get_feats_constiutuents(trained_model.dataset.source)
+    if plot_source: jet0 = get_feats_constiutuents(trained_model.dataset.source)
     jet1 = get_feats_constiutuents(trained_model.pipeline.trajectories[-1])
     jet_target = get_feats_constiutuents(trained_model.dataset.target)
 
-    fig, ax = plt.subplots(1, 4, figsize=figsize)
+    fig = plt.figure(figsize=figsize)
+    gs = gridspec.GridSpec(2, len(features), height_ratios=[5, 1])
+    gs.update(hspace=0.05) 
+    
+    for i in range(len(features)):
+        ax = fig.add_subplot(gs[i])
+        h, b, _ = ax.hist(jet_target[...,i], bins=bins[i], log=True, color='silver', histtype='stepfilled', density=True, lw=0.75, label='target (truth)')
+        if plot_source: ax.hist(jet0[...,i], bins=bins[i], log=True, color=color[i], histtype='step', ls='--', density=True, lw=0.75, label='source (t=0)')
+        h1, _, _ = ax.hist(jet1[...,i], bins=bins[i], log=True, color=['gold', 'darkblue', 'darkred', 'purple'][i], histtype='step', density=True, lw=0.75, label='gen. target (t=1)')
 
-    for i in range(4):
-        ax[i].hist(jet0[...,i], bins=bins[i], log=True, color=['gold', 'darkblue', 'darkred', 'purple'][i], histtype='step', ls='--', density=True, lw=0.75, label='source (t=0)')
-        ax[i].hist(jet1[...,i], bins=bins[i], log=True, color=['gold', 'darkblue', 'darkred', 'purple'][i], histtype='step', density=True, lw=0.75, label='gen. target (t=1)')
-        ax[i].hist(jet_target[...,i], bins=bins[i], log=True, color='silver', histtype='stepfilled', density=True, lw=0.75, label='target (truth)')
-        ax[i].set_xlabel(features[i])
-        ax[i].legend(fontsize=6)
-        for tick in ax[i].yaxis.get_major_ticks():
-            tick.label.set_fontsize(8)
-        for tick in ax[i].xaxis.get_major_ticks():
-            tick.label.set_fontsize(8)
+        ax.set_xticklabels([])
+        ax.set_xticks([])
+        for tick in ax.yaxis.get_major_ticks(): tick.label.set_fontsize(8)
+        ax.legend(fontsize=6, loc='upper right')
+        
+        # Ratio plot:
+        ax_ratio = fig.add_subplot(gs[i + 4])
+        ratio = np.divide(h1, h, out=np.ones_like(h), where=h != 0)
+        ax_ratio.plot(0.5 * (b[:-1] + b[1:]), ratio, drawstyle='steps-post', color=color[i],lw=0.75)
+        ax_ratio.set_ylim(0.5, 1.5, 0) # Adjust this as needed
+        ax_ratio.set_xlabel(features[i] + ' jet-level')
+        ax_ratio.axhline(1, color='gray', linestyle='--', lw=0.75)
+        for tick in ax_ratio.xaxis.get_major_ticks(): tick.label.set_fontsize(7)
+        for tick in ax_ratio.yaxis.get_major_ticks(): tick.label.set_fontsize(5)  
+        if i == 0: ax_ratio.set_ylabel('ratio', fontsize=8)
+        ax_ratio.set_yticks([0.5, 1, 1.5])
+
     if save_dir is not None:
         plt.savefig(save_dir + '/jet_features.pdf')
     else:
